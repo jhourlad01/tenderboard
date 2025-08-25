7fc9f7136477a27ec88276e60c7c894a48732b4c
Joe Estrella <jhourlad01@gmail.com>
2025-08-25 10:08:28 +0800

    Add mocha and typescript support

---
+++ code/dist/test-setup.test.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = require("chai");
+describe('Test Setup', () => {
+    it('should work with TypeScript and Mocha', () => {
+        (0, chai_1.expect)(true).to.be.true;
+    });
+    it('should handle basic math', () => {
+        (0, chai_1.expect)(2 + 2).to.equal(4);
+    });
+});
+//# sourceMappingURL=test-setup.test.js.map
